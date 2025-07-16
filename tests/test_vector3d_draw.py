398478cf273a820dba041ee4948c129b0ab4a54e
shotadft <98450322+shotadft@users.noreply.github.com>
2025-07-16 11:26:19 +0900

    Restore Tests

---
+++ tests/test_vector3d_draw.py
@@ -0,0 +1,56 @@
+import os
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from package.vecposlib.positionlib import Position
+from package.vecposlib.vectorlib import Vec3
+
+def draw_vector3d(radius: int, pos: Position[int], outdir: str = "") -> str:
+    if not outdir:
+        outdir = os.path.expanduser("~/Desktop")
+    
+    vec = Vec3(pos)
+    
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    fig.add_axes(ax)
+
+    ax.grid(True)
+    ax.quiver(0, 0, 0, vec.x, vec.y, vec.z, color='r', arrow_length_ratio=0.1)
+    ax.scatter(0, 0, 0, color='k', marker='o')
+    ax.scatter(vec.x, vec.y, vec.z, color='k', marker='o')
+
+    ax.set_xlim(-radius, radius)
+    ax.set_ylim(-radius, radius)
+    ax.set_zlim(-radius, radius)
+
+    ax.set_xticks(range(-radius, radius + 1))
+    ax.set_yticks(range(-radius, radius + 1))
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    
+    plt.show()
+
+    outpath = os.path.join(outdir, f"vector3d_r{radius}_({vec.x},{vec.y},{vec.z}).png")
+    fig.savefig(outpath)
+    plt.close(fig)
+
+    return outpath
+
+if __name__ == "__main__":
+    try:
+        MAX_RADIUS = 30
+        radius = int(input(f"グリッド半径を1~{MAX_RADIUS}で指定してください: "))
+        if not (0 < radius <= MAX_RADIUS):
+            raise ValueError
+        x = int(input(f"x(整数: -{radius}~{radius}): "))
+        y = int(input(f"y(整数: -{radius}~{radius}): "))
+        z = int(input(f"z(整数: -{radius}~{radius}): "))
+
+        pos = Position(x, y, z)
+        print(f"Position: {pos}")
+        out = draw_vector3d(radius, pos)
+        print(f"Exported: {out}")
+    except Exception as e:
+        print(f"入力エラー: {e}")
---
+++ tests/test_vector_draw.py
@@ -0,0 +1,40 @@
+import os
+import matplotlib.pyplot as plt
+from package.vecposlib.positionlib import Position
+from package.vecposlib.vectorlib import Vec2
+
+def draw_vector(radius: int, pos: Position[int], outdir: str = "") -> str:
+    if not outdir:
+        outdir = os.path.expanduser("~/Desktop")
+    
+    vec = Vec2(pos.x, pos.y)
+    
+    fig, ax = plt.subplots()
+    ax.grid(True)
+    ax.plot([0, vec.x], [0, vec.y], marker="o")
+    ax.arrow(0, 0, vec.x, vec.y, head_width=0.2, head_length=0.3, fc="r", ec="r")
+    ax.set_xticks(range(-radius, radius + 1))
+    ax.set_yticks(range(-radius, radius + 1))
+    ax.set_xlim(-radius, radius)
+    ax.set_ylim(-radius, radius)
+    plt.show()
+    outpath = os.path.join(outdir, f"vector_r{radius}_({vec.x},{vec.y}).png")
+    fig.savefig(outpath)
+    plt.close(fig)
+    return outpath
+
+if __name__ == "__main__":
+    try:
+        MAX_RADIUS = 256
+        radius = int(input(f"グリッド半径を1~{MAX_RADIUS}で指定してください: "))
+        if not (0 < radius <= MAX_RADIUS):
+            raise ValueError
+        x = int(input(f"x(整数: -{radius}~{radius}): "))
+        y = int(input(f"y(整数: -{radius}~{radius}): "))
+
+        pos = Position(x, y)
+        print(f"Position: {pos}")
+        out = draw_vector(radius, pos)
+        print(f"Exported: {out}")
+    except Exception as e:
+        print(f"入力エラー: {e}")
